b58769d29cdb52a83d33aa0cc1d63ef38229222c
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-04 00:22:02 +0000

    Added code

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: May 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function integersSent() {
+  
+  //initialize variable for sum
+  let sum = 0;
+
+  //getting the user's two inputted numbers that will be used to calculate the product
+  let firstNum = parseInt(document.getElementById("first-number").value);
+  let secondNum = parseInt(document.getElementById("second-number").value);
+
+  //for loop to do repeated addition to find the product
+  for (let counter = 1; counter <= secondNum; counter++) {
+    sum = sum + firstNum;
+  }
+  //displaying the sum/product to the screen
+  document.getElementById("results").innerHTML = "The product of your selected two numbers is " + sum + ".";
+}
