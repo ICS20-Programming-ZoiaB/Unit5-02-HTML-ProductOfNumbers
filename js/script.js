b6067306bf66de285c4df39c44537cbd1696d7cf
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-11 02:24:43 +0000

    Added else statement

--- js/script.js
+++ js/script.js
@@ -18,21 +18,17 @@ function integersSent() {
     document.getElementById("results").innerHTML = "Please enter valid numbers.";
     return;
   }
-
-  // Determine whether the product is positive or negative
-  let isNegative = false;
-  if ((firstNum < 0 && secondNum > 0) || (firstNum > 0 && secondNum < 0)) {
-    isNegative = true;
-  }
-
-  // Take the absolute value to do multiplication
-  let firstNumAbsolute = Math.abs(firstNum);
-  let secondNumAbsolute = Math.abs(secondNum);
-
-  //For loop to execute repeated addition that will result in the product.
-  for (let counter = 0; counter < secondNumAbsolute; counter++) {
-    product = product + firstNumAbsolute;
-  }
+  
+  //Else (for if numbers are entered)
+  else {
+    // Take the absolute value to do multiplication
+    let firstNumAbsolute = Math.abs(firstNum);
+    let secondNumAbsolute = Math.abs(secondNum);
+    //For loop to execute repeated addition that will result in the product.
+    for (let counter = 0; counter < secondNumAbsolute; counter++) {
+      product = product + firstNumAbsolute;
+    }
+      }
 
   //If statement to make sure that if one of the numbers is negative, the product will be negative
   if ((firstNum < 0 && secondNum > 0) || (firstNum > 0 && secondNum < 0)) {
